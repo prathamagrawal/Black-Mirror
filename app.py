99cc62afec8edfd24cbcbae69a0d90853886aaf4
Pratham Agrawal <prathamagrawal1205@gmail.com>
2022-11-06 02:15:17 +0530

    Integrated ML Model

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, flash, request, redirect, url_for, render_template
 import urllib.request
 import os
 from werkzeug.utils import secure_filename
+import argparse
+import matplotlib.pyplot as plt
+from colorizers import *
+
 
 app = Flask(__name__)
 
@@ -10,9 +14,11 @@ UPLOAD_FOLDER = 'static/uploads/'
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
+# load colorizers
+colorizer_eccv16 = eccv16(pretrained=True).eval()
+colorizer_siggraph17 = siggraph17(pretrained=True).eval()
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -39,7 +45,19 @@ def upload_image():
         print('upload_image filename: ' + filename)
         flash('Image successfully uploaded and displayed below')
 
+        # default size to process images is 256x256
+        # grab L channel in both original ("orig") and resized ("rs") resolutions
+        img = load_img("./static/uploads/"+filename)
+        (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
 
+        # colorizer outputs 256x256 ab map
+        # resize and concatenate to original L channel
+        img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
+        out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
+        out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+
+        plt.imsave('eccv16.png', out_img_eccv16)
+        plt.imsave('%siggraph17.png', out_img_siggraph17)
 
 
         return render_template('index.html', filename=filename,output_sigg=filename,output_eccv=filename)
